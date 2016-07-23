4a3e929b57d8942369c114356f5bb84c525bc399
droo5ki <etch.himself@gmail.com>
2016-07-23 09:26:53 -0700

    Added .gitignore, initial commit of parser.py.

---
+++ parser.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#########################################################
+#
+# parser.py: reimplementation of parser.pl in python
+# 
+# Original parser.pl Copyright 2013 George D. Nincehelser
+# 
+#########################################################
+
+class Parser(object):
+
+    def __init__():
+         celsius, farenheit, humidity, windspeed, winddir, rainfall, pressure,
+         presssure_in = '0'
+
+if __name__ == '__main__':
